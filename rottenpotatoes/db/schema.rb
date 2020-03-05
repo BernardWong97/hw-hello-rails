3e7acc0b7aa7b84926560b63812196ddaf00d2bf
Bernard Wong <ben.wc88@gmail.com>
2020-03-05 23:18:30 +0000

    Part 3 - create routes actions and views

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -14,6 +14,12 @@
 ActiveRecord::Schema.define(version: 20200304020240) do
 
   create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.text     "description"
+    t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
